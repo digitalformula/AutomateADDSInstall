ab66e17f6fa1fd8ce94f981673b5ab1fdcf4d542
Chris Rasmussen <chris@chris-nutanix.local>
2017-06-23 12:50:08 +1000

    Change log function name

--- configureAdObjects.ps1
+++ configureAdObjects.ps1
@@ -1,6 +1,6 @@
 $Logfile = "C:\Logs\$(gc env:computername)-adObjects.log"
 
-Function LogWrite
+Function LogWriteObjectsObjects
 {
    Param ([string]$logstring)
    Add-content $Logfile -value $logstring
@@ -8,16 +8,16 @@ Function LogWrite
 
 $Server = $env:COMPUTERNAME
 
-LogWrite("Creating AD groups ...")
+LogWriteObjects("Creating AD groups ...")
 New-ADGroup -Server $Server -Name "PrismAdmins" -GroupScope DomainLocal
 New-ADGroup -Server $Server -Name "SSPAdmins" -GroupScope DomainLocal
 New-ADGroup -Server $Server -Name "SSPUsers" -GroupScope DomainLocal
 
-LogWrite("Creating AD users ...")
+LogWriteObjects("Creating AD users ...")
 New-ADUser -Server $Server "Nutanix User" -SAMAccountName "nutanix.user" -DisplayName "Nutanix User" -GivenName "Nutanix" -Surname "User" -UserPrincipalName "nutanix.user@local.ntnxdemo7.com" -PasswordNeverExpires:$true -AccountPassword (ConvertTo-SecureString -AsPlainText "Ntnx2017!" -Force) -Enabled:$true
 New-ADUser -Server $Server "Jane Doe" -SAMAccountName "jane.doe" -DisplayName "Jane Doe" -GivenName "Jane" -Surname "Doe" -UserPrincipalName "jane.doe@local.ntnxdemo7.com" -PasswordNeverExpires:$true -AccountPassword (ConvertTo-SecureString -AsPlainText "Ntnx2017!" -Force) -Enabled:$true
 
-LogWrite("Adding admin user to groups ...")
+LogWriteObjects("Adding admin user to groups ...")
 $prismGroup = Get-ADGroup -Server $Server "CN=PrismAdmins,CN=Users,DC=local,DC=ntnxdemo7,DC=com"
 $sspAdminsGroup = Get-ADGroup -Server $Server "CN=SSPAdmins,CN=Users,DC=local,DC=ntnxdemo7,DC=com"
 $sspUsersGroup = Get-ADGroup -Server $Server "CN=SSPUsers,CN=Users,DC=local,DC=ntnxdemo7,DC=com"
@@ -25,12 +25,12 @@ $user = Get-ADUser -Server $Server "CN=Nutanix User,CN=Users,DC=local,DC=ntnxdem
 Add-ADGroupMember -Server $Server $prismGroup -Members $user
 Add-ADGroupMember -Server $Server $SSPAdminsGroup -Members $user
 
-LogWrite("Adding non-admin user to groups ...")
+LogWriteObjects("Adding non-admin user to groups ...")
 $sspUsersGroup = Get-ADGroup -Server $Server "CN=SSPUsers,CN=Users,DC=local,DC=ntnxdemo7,DC=com"
 $user = Get-ADUser -Server $Server "CN=Jane Doe,CN=Users,DC=local,DC=ntnxdemo7,DC=com"
 Add-ADGroupMember -Server $Server $sspUsersGroup -Members $user
 
-LogWrite("Removing scheduled task ...")
+LogWriteObjects("Removing scheduled task ...")
 Unregister-ScheduledTask "ConfigureAD" -Confirm:$false
 
-LogWrite("Done!")
+LogWriteObjects("Done!")

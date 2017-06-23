40855b582917d2f6886edd6ddb532c650f5d064c
Chris Rasmussen <chris@chris-nutanix.local>
2017-06-23 13:32:59 +1000

    Add Start-Transcript to staging script, increment domain number

--- configureAd.ps1
+++ configureAd.ps1
@@ -14,7 +14,7 @@ Start-Sleep -s 300
 LogWrite("Setting up credentials ...")
 $password = ConvertTo-SecureString "nutanix/4u" -AsPlainText -Force
 LogWrite("Password created")
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "ntnxdemo9\administrator",$password
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "ntnxdemo10\administrator",$password
 LogWrite("Credential created")
 
 LogWrite("Running AD object creation script ...")
--- installAd.ps1
+++ installAd.ps1
@@ -1,3 +1,6 @@
+mkdir C:\Logs
+Start-Transcript -Path "C:\Logs\stage-transcript.txt"  -IncludeInvocationHeader -Force
+
 Write-Host 'Creating post-reboot scheduled task ...'
 $configureAdAction = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-ExecutionPolicy Bypass -File ""C:\Scripts\configureAd.ps1"""
 # $configureAdUserName = New-ScheduledTaskPrincipal -UserID "Administrator" -LogonType Password -RunLevel Highest
@@ -13,7 +16,6 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 choco install git.install -y -params "/GitAndUnixToolsOnPath"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
-mkdir C:\Logs
 mkdir C:\Scripts
 git clone https://github.com/digitalformula/AutomateADDSInstall.git C:\Scripts -q
 
@@ -24,4 +26,4 @@ Add-WindowsFeature RSAT-ADDS-Tools
 Write-Host "Importing ADDS Deployment Module ..."
 Import-Module ADDSDeployment
 Write-Host "Creating AD Forest, Domain and first DC ..."
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "local.ntnxdemo9.com" -DomainNetbiosName "ntnxdemo9" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath: "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword (ConvertTo-SecureString -String "nutanix/4u" -AsPlainText -Force)
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "local.ntnxdemo10.com" -DomainNetbiosName "ntnxdemo10" -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath: "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword (ConvertTo-SecureString -String "nutanix/4u" -AsPlainText -Force)

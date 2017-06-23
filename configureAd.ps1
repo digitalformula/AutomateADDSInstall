afa4817f34dd31977918aa5ae9dc85fda4c22600
Chris Rasmussen <chris@chris-nutanix.local>
2017-06-23 15:52:11 +1000

    Latest test version

--- configureAd.ps1
+++ configureAd.ps1
@@ -1,28 +1,28 @@
 Start-Transcript -Path "C:\Logs\configureAd-transcript.txt" -IncludeInvocationHeader -Force
 
-$Logfile = "C:\Logs\$(gc env:computername)-stage.log"
+# $Logfile = "C:\Logs\$(gc env:computername)-stage.log"
 
-Function LogWrite
-{
-   Param ([string]$logstring)
-   Add-content $Logfile -value $logstring
-}
+# Function LogWrite
+# {
+#    Param ([string]$logstring)
+#    Add-content $Logfile -value $logstring
+# }
 
-LogWrite("Waiting 5 minutes ...")
+# LogWrite("Waiting 5 minutes ...")
 Start-Sleep -s 300
 
-LogWrite("Setting up credentials ...")
+# LogWrite("Setting up credentials ...")
 # $password = ConvertTo-SecureString "nutanix/4u" -AsPlainText -Force
 # LogWrite("Password created")
 # $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "ntnxdemo10\administrator",$password
 $credential = New-Object System.Management.Automation.PSCredential ('ntnxdemo10\administrator', (ConvertTo-SecureString 'nutanix/4u' -AsPlainText -Force))
-LogWrite("Credential created")
+# LogWrite("Credential created")
 
-LogWrite("Running AD object creation script ...")
+# LogWrite("Running AD object creation script ...")
 Start-Process powershell.exe -Credential $credential -NoNewWindow -ArgumentList "-File C:\Scripts\configureAdObjects.ps1"
 
 # Start-Process powershell -Credential $pp -ArgumentList '-noprofile -command &{Start-Process $script -verb runas}'
 
-LogWrite("Process finished!")
+# LogWrite("Process finished!")
 
 Stop-Transcript
--- configureAdObjects.ps1
+++ configureAdObjects.ps1
@@ -1,25 +1,25 @@
 Start-Transcript -Path "C:\Logs\configureAdOjects-transcript.txt"  -IncludeInvocationHeader -Force
 
-$Logfile = "C:\Logs\$(gc env:computername)-adObjects.log"
+# $Logfile = "C:\Logs\$(gc env:computername)-adObjects.log"
 
-Function LogWriteObjects
-{
-   Param ([string]$logstring)
-   Add-content $Logfile -value $logstring
-}
+# Function LogWriteObjects
+# {
+#    Param ([string]$logstring)
+#    Add-content $Logfile -value $logstring
+# }
 
 $Server = $env:COMPUTERNAME
 
-LogWriteObjects("Creating AD groups ...")
+# LogWriteObjects("Creating AD groups ...")
 New-ADGroup -Server $Server -Name "PrismAdmins" -GroupScope DomainLocal
 New-ADGroup -Server $Server -Name "SSPAdmins" -GroupScope DomainLocal
 New-ADGroup -Server $Server -Name "SSPUsers" -GroupScope DomainLocal
 
-LogWriteObjects("Creating AD users ...")
+# LogWriteObjects("Creating AD users ...")
 New-ADUser -Server $Server "Nutanix User" -SAMAccountName "nutanix.user" -DisplayName "Nutanix User" -GivenName "Nutanix" -Surname "User" -UserPrincipalName "nutanix.user@local.ntnxdemo10.com" -PasswordNeverExpires:$true -AccountPassword (ConvertTo-SecureString -AsPlainText "Ntnx2017!" -Force) -Enabled:$true
 New-ADUser -Server $Server "Jane Doe" -SAMAccountName "jane.doe" -DisplayName "Jane Doe" -GivenName "Jane" -Surname "Doe" -UserPrincipalName "jane.doe@local.ntnxdemo10.com" -PasswordNeverExpires:$true -AccountPassword (ConvertTo-SecureString -AsPlainText "Ntnx2017!" -Force) -Enabled:$true
 
-LogWriteObjects("Adding admin user to groups ...")
+# LogWriteObjects("Adding admin user to groups ...")
 $prismGroup = Get-ADGroup -Server $Server "CN=PrismAdmins,CN=Users,DC=local,DC=ntnxdemo10,DC=com"
 $sspAdminsGroup = Get-ADGroup -Server $Server "CN=SSPAdmins,CN=Users,DC=local,DC=ntnxdemo10,DC=com"
 $sspUsersGroup = Get-ADGroup -Server $Server "CN=SSPUsers,CN=Users,DC=local,DC=ntnxdemo10,DC=com"
@@ -27,14 +27,14 @@ $user = Get-ADUser -Server $Server "CN=Nutanix User,CN=Users,DC=local,DC=ntnxdem
 Add-ADGroupMember -Server $Server $prismGroup -Members $user
 Add-ADGroupMember -Server $Server $SSPAdminsGroup -Members $user
 
-LogWriteObjects("Adding non-admin user to groups ...")
+# LogWriteObjects("Adding non-admin user to groups ...")
 $sspUsersGroup = Get-ADGroup -Server $Server "CN=SSPUsers,CN=Users,DC=local,DC=ntnxdemo10,DC=com"
 $user = Get-ADUser -Server $Server "CN=Jane Doe,CN=Users,DC=local,DC=ntnxdemo10,DC=com"
 Add-ADGroupMember -Server $Server $sspUsersGroup -Members $user
 
-LogWriteObjects("Removing scheduled task ...")
+# LogWriteObjects("Removing scheduled task ...")
 Unregister-ScheduledTask "ConfigureAD" -Confirm:$false
 
-LogWriteObjects("Done!")
+# LogWriteObjects("Done!")
 
 Stop-Transcript

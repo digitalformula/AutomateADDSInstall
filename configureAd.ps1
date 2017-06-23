b8625599eeef19a364962726d05c59a4781af4f0
Chris Rasmussen <chris@chris-nutanix.local>
2017-06-23 11:37:29 +1000

    Update domain name

--- configureAd.ps1
+++ configureAd.ps1
@@ -11,7 +11,7 @@ Start-Sleep -s 120
 
 LogWrite("Setting up credentials ...")
 $password = ConvertTo-SecureString "nutanix/4u" -AsPlainText -Force
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "ntnxdemo2\administrator",$password
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "ntnxdemo5\administrator",$password
 
 LogWrite("Running AD object creation script ...")
 Start-Process powershell.exe -Credential $credential -NoNewWindow -ArgumentList "-File C:\Scripts\configureAdObjects.ps1"
